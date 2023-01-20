f9cdf57b3aee986572c6af4103c606c2811e4cfe
Mayk Brito <maykbrito@gmail.com>
2023-01-20 18:24:39 -0300

    add routes/ dalle working

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,11 @@
+import express from "express"
+import * as dotenv from "dotenv"
+import { v2 as cloudinary } from "cloudinary"
+
+import Post from "../mongodb/models/Post.js"
+
+dotenv.config()
+
+const router = express.Router()
+
+export default router
