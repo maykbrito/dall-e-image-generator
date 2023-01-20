5f41a8249e705069e0f4be1192591ae9e6f7ef73
Mayk Brito <maykbrito@gmail.com>
2023-01-20 19:17:47 -0300

    add downloadImage func to utils

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,3 +1,5 @@
+import FileSaver from "file-saver"
+
 import { surpriseMePrompts } from "../constants"
 
 export function getRandomPrompt(prompt: string): string {
@@ -7,4 +9,8 @@ export function getRandomPrompt(prompt: string): string {
   if (randomPrompt === prompt) return getRandomPrompt(prompt)
 
   return randomPrompt
+}
+
+export async function downloadImage(_id: string, photo: string) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`)
 }
